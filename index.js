faedb248704e32ca28fedb641728c4e169ebc856
kks4866 <85468854+kks4866@users.noreply.github.com>
2024-10-24 12:02:01 +0900

    처음 올리는 동물 소리입니다.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.get('/sound/:name', function (req, res) {
+    const {name} =req.params
+    if(name == "dog"){
+        res.send({'sound':'멍멍'})
+    } else if(name == "cat"){
+        res.send({'sound':'야옹'})
+    } else if(name == "pig"){
+        res.send({'sound':'꿀꿀'})
+    } else {
+        res.send({'sound':'알수없습'})    
+    }
+        
+
+    
+})
+app.listen(3000)
